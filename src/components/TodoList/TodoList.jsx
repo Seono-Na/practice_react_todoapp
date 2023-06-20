20c778447491ae25f339de969d767bc0ae3e9bf3
Seono-Na <seono5670@gmail.com>
2023-06-20 22:02:28 +0900

    Add To Do

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
+import AddTodo from "../AddTodo/AddTodo";
 
 export default function TodoList() {
   const [todos, setTodos] = useState([
     { id: "001", text: "독서하기", status: "active" },
     { id: "002", text: "글쓰기", status: "active" },
   ]);
+  const handleAdd = (todo) => setTodos([...todos, todo]);
   return (
     <section>
       <ul>
@@ -12,6 +14,7 @@ export default function TodoList() {
           <li key={item.id}>{item.text}</li>
         ))}
       </ul>
+      <AddTodo onAdd={handleAdd} />
     </section>
   );
 }
