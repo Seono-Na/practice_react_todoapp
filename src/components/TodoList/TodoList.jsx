94255304bea44a28a3585cf71e82fa73564bc5bb
Seono-Na <seono5670@gmail.com>
2023-06-20 23:15:58 +0900

    Update To Do Checked

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,17 +1,27 @@
 import React, { useState } from "react";
 import AddTodo from "../AddTodo/AddTodo";
+import Todo from "../Todo/Todo";
+import { v4 as uuidv4 } from "uuid";
 
 export default function TodoList() {
   const [todos, setTodos] = useState([
-    { id: "001", text: "독서하기", status: "active" },
-    { id: "002", text: "글쓰기", status: "active" },
+    { id: uuidv4(), text: "독서하기", status: "active" },
+    { id: uuidv4(), text: "글쓰기", status: "active" },
   ]);
   const handleAdd = (todo) => setTodos([...todos, todo]);
+  const handleUpdate = (updated) =>
+    setTodos(todos.map((item) => (item.id === updated.id ? updated : item)));
+  const handleDelete = () => {};
   return (
     <section>
       <ul>
         {todos.map((item) => (
-          <li key={item.id}>{item.text}</li>
+          <Todo
+            key={item.id}
+            todo={item}
+            onUpdate={handleUpdate}
+            onDelete={handleDelete}
+          />
         ))}
       </ul>
       <AddTodo onAdd={handleAdd} />
